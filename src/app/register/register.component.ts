20aa4dcfdc27a3e45ced24e6932f79468794509d
h.3ziz <h.3ziz98@gmail.com>
2022-02-08 05:41:24 +0200

    Day6-Lab6

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenService } from '../authen.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerInfo = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    hearAboutUs: ''
+  }
+  constructor(private AuthenService: AuthenService) { }
 
   ngOnInit(): void {
   }
+  onRegister(){
+    this.AuthenService.register(this.registerInfo).subscribe(
+      {
+        next:(result)=>{
+          console.log(result)
 
+          localStorage.setItem("user",JSON.stringify({email:this.registerInfo.email, passsword:this.registerInfo.password, name:this.registerInfo.name}))
+        },
+        error:(err)=> console.log(err)
+      }
+    )
+  }
 }
